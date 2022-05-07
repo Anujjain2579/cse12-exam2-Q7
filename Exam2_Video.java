d2c23bda2b937e7dba216a2d5f598ae68a981587
Anujjain2579 <103171741+Anujjain2579@users.noreply.github.com>
2022-05-07 01:00:28 -0700

    Update Exam2_Video.java

--- Exam2_Video.java
+++ Exam2_Video.java
@@ -20,20 +20,20 @@ class Video_Task {
     Big O refers to the tight bound in this question
     |-----------------------------------------------|
     | Line                      | Number of Steps   |
-    | long accum = 0            |                   |
-    | int mult = 0              |                   |
-    | int i = s.length() - 1    |                   |
-    | i > -1                    |                   |
-    | i--                       |                   |
-    | char digit = s.charAt(i)  |                   |
-    | accum += ...              |                   |
-    | mult++                    |                   |
-    | return accum              |                   |
+    | long accum = 0            |       1           |
+    | int mult = 0              |       1            |
+    | int i = s.length() - 1    |       1            |
+    | i > -1                    |       n+1            |
+    | i--                       |       n            |
+    | char digit = s.charAt(i)  |       n            |
+    | accum += ...              |       n            |
+    | mult++                    |       n            |
+    | return accum              |       1            |
     |-----------------------------------------------|
 
-    Runtime Equation: ____
+    Runtime Equation: 5n+5
 
-    Total runtime: O(___)
+    Total runtime: O(n)
     */
 
     public static int[] method2(int[] arr) {
@@ -59,21 +59,21 @@ class Video_Task {
 
     |-----------------------------------------------|
     | Line                      | Number of Steps   |
-    | int i = 0                 |                   |
-    | i < arr.length            |                   |
-    | i++                       |                   |
-    | if (i % 3 == 0)           |                   |
-    | int amount = 1            |                   |
-    | int j = 0                 |                   |
-    | j < arr.length / 2        |                   |
-    | j++                       |                   |
-    | amount *= arr[i]          |                   |
-    | arr[i] = amount           |                   |
-    | return arr                |                   |
+    | int i = 0                 |       1            |
+    | i < arr.length            |       n+1            |
+    | i++                       |       n            |
+    | if (i % 3 == 0)           |       n            |
+    | int amount = 1            |       1+ (n-1)/3 = (n+2)/3            |
+    | int j = 0                 |       (n+2)/3            |
+    | j < arr.length / 2        |       (n+2)/3             |
+    | j++                       |       (n+2)/3 (n/2 +1)            |
+    | amount *= arr[i]          |       (n+2)/3 (n/2)            |
+    | arr[i] = amount           |       (n+2)/3 (n/2)           |
+    | return arr                |        1           |
     |-----------------------------------------------|
     
-    Runtime Equation: ____
+    Runtime Equation: {3n^2 +32n +34} /6
 
-    Total runtime: O(___)
+    Total runtime: O(n^2)
     */
 }
